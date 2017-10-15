642d17c4970464a57616383d0456028a7d1f731f
rijal rebelworks <rijal.asep@rebelworks.co>
2017-10-15 16:59:02 +0100

    users module

--- app.js
+++ app.js
@@ -22,8 +22,17 @@ app.use(session({ secret: 'keyboard cat',
 app.use(flash())
 
 // to get post data on body
-const bodyParser = require('body-parser')
+const bodyParser      = require('body-parser')
+const methodOverride  = require('method-override')
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+    // look in urlencoded POST bodies and delete it
+    var method = req.body._method
+    delete req.body._method
+    return method
+  }
+}))
 
 // routing
 require('./route/router')(app)
--- controllers/UserController.js
+++ controllers/UserController.js
@@ -78,20 +78,20 @@ exports.Create = function(request, response){
   }
   else {
     User.Create(request, function(err, res, body){
-      if(res.statusCode == 200) {
+      if(res.statusCode == 201) {
         request.flash('info', 'User has been created')
         response.redirect('/users')
       }
       else {
         request.flash('error', body)
-        console.log(request)
+        //console.log(request)
         response.title              = "User Management | Create New User"
         response.flash_info         = request.flash('info')
         response.flash_error        = request.flash('error')
         response.flash_warning      = request.flash('warning')
         response.form               = request.body
         response.form.group_id      = parseInt(response.form.group_id)
-        response.form.city_id       = parseInt(response.form.city_id)                
+        response.form.city_id       = parseInt(response.form.city_id)
 
         Group.GetGroups(request, function(err, res, body){
           response.groups           = JSON.parse(body)
@@ -101,54 +101,90 @@ exports.Create = function(request, response){
           })
         })
       }
-
-
     })
   }
 }
 
 exports.EditForm = function(request, response){
-  if (request.session.token && request.session.token != 'undefined') {
-    response.redirect('/')
+  if (!request.session.token || request.session.token == 'undefined') {
+    response.redirect('/login')
   }
   else {
-    response.flash_info = request.flash('info')
-    response.flash_error = request.flash('error')
-    response.flash_warning = request.flash('warning')
-    response.layout = false
-    response.render('user/Login', response)
+    User.Get(request, function(err, res, body){
+      if(res.statusCode == 200) {
+        response.title              = "User Management | Edit User"
+        response.flash_info         = request.flash('info')
+        response.flash_error        = request.flash('error')
+        response.flash_warning      = request.flash('warning')
+        response.form               = JSON.parse(body)
+        //console.log(response.form)
+        Group.GetGroups(request, function(err, res, body){
+          response.groups           = JSON.parse(body)
+          City.GetCities(request, function(err, res, body){
+            response.cities         = JSON.parse(body)
+            response.render('user/Edit', response)
+          })
+        })
+      }
+      else {
+        request.flash('error', body)
+        response.redirect('/users')
+      }
+    })
   }
 }
 
 exports.Edit = function(request, response){
-  if (request.session.token && request.session.token != 'undefined') {
-    response.redirect('/')
+  if (!request.session.token || request.session.token == 'undefined') {
+    response.redirect('/login')
   }
   else {
-    Token.GetToken(request, function(err, res, body){
+    User.Edit(request, function(err, res, body){
       if(res.statusCode == 200) {
-        var bodies = body.split("\"")
-        request.session.token = bodies[1]
-        response.redirect('/')
+        request.flash('info', 'User has been updated')
+        console.log(body)
+        response.redirect('/users')
       }
       else {
         request.flash('error', body)
-        response.redirect('/login')
+        response.title              = "User Management | Edit User"
+        response.flash_info         = request.flash('info')
+        response.flash_error        = request.flash('error')
+        response.flash_warning      = request.flash('warning')
+        var formData = {
+          'Name'  : request.body.name,
+          'Phone' : request.body.phone,
+          'Group' : {'ID': parseInt(request.body.group_id)},
+          'City'  : {'ID': parseInt(request.body.city_id)}
+        }
+        response.form               = formData
+
+        Group.GetGroups(request, function(err, res, body){
+          response.groups           = JSON.parse(body)
+          City.GetCities(request, function(err, res, body){
+            response.cities         = JSON.parse(body)
+            response.render('user/Edit', response)
+          })
+        })
       }
     })
   }
 }
 
 exports.Delete = function(request, response){
-  if (request.session.token && request.session.token != 'undefined') {
-    response.redirect('/')
+  if (!request.session.token || request.session.token == 'undefined') {
+    response.redirect('/login')
   }
   else {
-    response.flash_info = request.flash('info')
-    response.flash_error = request.flash('error')
-    response.flash_warning = request.flash('warning')
-    response.layout = false
-    response.render('user/Login', response)
+    User.Delete(request, function(err, res, body){
+      if(res.statusCode == 204) {
+        request.flash('info', 'User has been deleted')
+      }
+      else {
+        request.flash('error', body)
+      }
+      response.redirect('/users')
+    })
   }
 }
 
